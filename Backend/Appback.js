3aa21094a2c56f794811451a421ddfb056bc036b
AminulIslam254 <lostvayne684@gmail.com>
2022-09-07 21:16:56 +0530

    Update Appback.js

--- Backend/Appback.js
+++ Backend/Appback.js
@@ -5,15 +5,15 @@ const app = express();
 const port = 5000;
 
 
-const {connectMongoose,User}= require("./Database/Connectdb.js");
- 
+const { connectMongoose, User } = require("./Database/Connectdb.js");
+
 connectMongoose();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("This is home page");
 })
 
@@ -21,32 +21,56 @@ app.post('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.get("/register",(req,res)=>{
+
+app.post("/login", (req, res)=> {
+  const { email, password} = req.body;
+  User.findOne({ email: email}, (err, user) => {
+      if(user){
+          if(password === user.password ) {
+              res.send({message: "Login Successfull", user: user})
+          } else {
+              res.send({ message: "Password didn't match"})
+          }
+      } else {
+          res.send({message: "User not registered"})
+      }
+  })
+}) 
+
+
+app.get("/register", (req, res) => {
   res.send("This is registe2");
 })
-app.post('/register', (req, res) => {
-    
-    console.log("lol");
-    console.log(req.body);
-    const {username,email,password}=req.body;
-    // User.findOne({password:password},(err,user)=>{
-    //     console.log("This is ",user);
-    // })
-    const user1=new User({
-      username,
-      email,
-      password
-    });
-    user1.save((err)=>{
-      if(err){
-        res.send(err);
-      }
-      else{
-        res.send("Registered Successfully");
-      }
-    });
-    
+
+
+
+app.post("/register", (req, res) => {
+  const {  email,username, password } = req.body
+  
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      
+      res.send({ message: "User already registerd" })
+    } else {
+      
+      const user = new User({
+        email,
+        username,
+        password
+      })
+      user.save(err => {
+        if (err) {
+          res.send(err)
+        } else {
+          res.send({ message: "Successfully Registered, Please login now." })
+        }
+      })
+    }
+  })
+
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
